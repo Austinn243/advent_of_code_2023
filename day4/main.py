8b07065610f4a3462e86c364ee7fcd1ba06d3350
Austin Nelson <austinn243@gmail.com>
2025-01-15 19:42:50 -0700

    [feat]: Rework day 4 using new models
    Reworked the solution for day 4 to make use of a new scratchcard
    model for improved clarity. In the process, the functionality has
    been decomposed for better separation of concerns.

--- day4/main.py
+++ day4/main.py
@@ -6,20 +6,52 @@ https://adventofcode.com/2023/day/4
 
 import re
 from os import path
+from typing import NamedTuple
 
 INPUT_FILE = "input.txt"
 
 CARD_ITEMS_REGEX = re.compile(r"(\d+)(?!\d*:)|\|")
+CARD_REGEX = re.compile(r"Card\s+(\d+): (.*)")
 
 
-def extract_numbers(line: str) -> tuple[set[int], set[int]]:
-    """Extract the winning numbers and selected numbers from the given line."""
+class Scratchcard(NamedTuple):
+    """Represents a scratchcard with winning and selected numbers."""
+
+    id: int
+    selected_numbers: set[int]
+    winning_numbers: set[int]
+
+
+def read_scratchcards(file_path: str) -> list[Scratchcard]:
+    """Read scratchcard information from a file."""
+
+    with open(file_path, encoding="utf-8") as file:
+        return [parse_scratchcard(line.strip()) for line in file]
+
+
+def parse_scratchcard(line: str) -> Scratchcard:
+    """Parse a scratchcard from a line of text."""
+
+    scratchcard_pattern_match = re.match(CARD_REGEX, line)
+    if not scratchcard_pattern_match:
+        raise ValueError(f"Invalid scratchcard line: {line}")
+
+    scratchcard_id = int(scratchcard_pattern_match.group(1))
+    selected_numbers, winning_numbers = parse_numbers(
+        scratchcard_pattern_match.group(2),
+    )
+
+    return Scratchcard(scratchcard_id, selected_numbers, winning_numbers)
+
+
+def parse_numbers(segment: str) -> tuple[set[int], set[int]]:
+    """Parse the winning and selected numbers from a segment of text."""
 
     winning_numbers = set()
     selected_numbers = set()
 
     target_set = winning_numbers
-    items = re.findall(CARD_ITEMS_REGEX, line)
+    items = re.findall(CARD_ITEMS_REGEX, segment)
 
     for item in items:
         if item == "":
@@ -30,28 +62,24 @@ def extract_numbers(line: str) -> tuple[set[int], set[int]]:
     return winning_numbers, selected_numbers
 
 
-def count_matches(winning_numbers: set[int], selected_numbers: set[int]) -> int:
+def count_matches(scratchcard: Scratchcard) -> int:
     """Count the number of matches between the winning numbers and selected numbers."""
 
-    return len(winning_numbers.intersection(selected_numbers))
+    matches = set.intersection(
+        scratchcard.selected_numbers,
+        scratchcard.winning_numbers,
+    )
+    return len(matches)
 
 
-def count_matches_per_game(file_path: str) -> dict[int, int]:
-    """Count the number of matches for each game."""
+def count_matches_per_card(scratchcards: list[Scratchcard]) -> dict[int, int]:
+    """Count the number of matches for each scratchcard."""
 
-    matches_per_game = {}
-
-    with open(file_path, encoding="utf-8") as file:
-        for game_id, line in enumerate(file, start=1):
-            winning_numbers, selected_numbers = extract_numbers(line)
-            match_count = count_matches(winning_numbers, selected_numbers)
-            matches_per_game[game_id] = match_count
-
-    return matches_per_game
+    return {card.id: count_matches(card) for card in scratchcards}
 
 
 def calculate_total_points(matches_per_game: dict[int, int]) -> int:
-    """Calculate the total points earned from the given cards."""
+    """Calculate the total points earned given the number of matches per card."""
 
     total_points = 0
 
@@ -80,10 +108,15 @@ def main() -> None:
 
     file_path = path.join(path.dirname(__file__), INPUT_FILE)
 
-    matches_per_game = count_matches_per_game(file_path)
-    print(matches_per_game)
-    print(calculate_total_points(matches_per_game))
-    print(count_cards(matches_per_game))
+    scratchcards = read_scratchcards(file_path)
+
+    matches_per_card = count_matches_per_card(scratchcards)
+
+    total_points = calculate_total_points(matches_per_card)
+    print(f"The total points earned from the cards is {total_points}")
+
+    card_count = count_cards(matches_per_card)
+    print(f"The total number of cards given is {card_count}")
 
 
 if __name__ == "__main__":
