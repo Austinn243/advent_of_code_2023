f363dbc60f9a7d829d495606b894c11c0b2aa9f3
Austin Nelson <austinn243@gmail.com>
2025-01-18 20:48:40 -0700

    [feat-7-py]: Add ruleset type
    Added a new ruleset type that contains information about how the
    cards and hands should be interpreted. It has been implemented in
    place of the previous callable-based implementation and some
    progress towards the second part of the challenge has been made,
    though it still needs work.

--- day7/main.py
+++ day7/main.py
@@ -14,33 +14,11 @@ from typing import NamedTuple
 INPUT_FILE = "input.txt"
 TEST_FILE = "test.txt"
 
-STANDARD_RANKS = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "T": 10,
-    "J": 11,
-    "Q": 12,
-    "K": 13,
-    "A": 14,
-}
-
 
 class Card(NamedTuple):
     """Represents a card in Camel Cards."""
 
     rank: str
-
-
-def get_card_value(card: Card, ranks: dict[str, int]) -> int:
-    """Return the value of the given card."""
-
-    return ranks[card.rank]
 
 
 class HandType(IntEnum):
@@ -65,27 +43,42 @@ class Play(NamedTuple):
     bid: int
 
 
-def read_plays(file_path: str) -> list[Play]:
-    """Read the plays for a game of Camel Cards from the input file."""
+class Ruleset(NamedTuple):
+    """Represents a ruleset for Camel Cards."""
 
-    with open(file_path) as file:
-        return [parse_play(line) for line in file]
+    evaluate_hand: Callable[[Hand], HandType]
+    rank_values: dict[str, int]
 
 
-def parse_play(line: str) -> Play:
-    """Parse a play from the given line."""
+def get_card_value(card: Card, ruleset: Ruleset) -> int:
+    """Return the value of the given card."""
 
-    raw_cards, raw_bid = line.strip().split(" ")
+    return ruleset.rank_values[card.rank]
 
-    hand = tuple(Card(raw_card) for raw_card in raw_cards)
-    bid = int(raw_bid)
 
-    return Play(hand, bid)
+def compare_cards(
+    hand1: Hand,
+    hand2: Hand,
+    ruleset: Ruleset,
+) -> int:
+    """Compare two hands."""
+
+    hand1_type = ruleset.evaluate_hand(hand1)
+    hand2_type = ruleset.evaluate_hand(hand2)
+
+    if hand1_type != hand2_type:
+        return hand1_type - hand2_type
+
+    hand1_values = [get_card_value(card, ruleset) for card in hand1]
+    hand2_values = [get_card_value(card, ruleset) for card in hand2]
+    differences = (v1 - v2 for v1, v2 in zip(hand1_values, hand2_values))
+
+    return next((diff for diff in differences if diff != 0), 0)
 
 
 @cache
 def get_standard_hand_type(hand: Hand) -> HandType:
-    """Determine the type of the hand with the given cards under standard rules."""
+    """Determine the type of the hand using the standard rules."""
 
     card_counts = Counter(hand)
 
@@ -111,34 +104,109 @@ def get_standard_hand_type(hand: Hand) -> HandType:
 
 
 @cache
-def compare_cards(
-    hand1: Hand,
-    hand2: Hand,
-    get_hand_type: Callable[[Hand], HandType],
-) -> int:
-    """Compare two hands."""
+def get_wild_hand_type(hand: Hand) -> HandType:
+    """Determine the type of the hand using the rules where jacks are jokers."""
 
-    hand1_type = get_hand_type(hand1)
-    hand2_type = get_hand_type(hand2)
+    card_counts = Counter(hand)
 
-    if hand1_type != hand2_type:
-        return hand1_type - hand2_type
+    # FIXME: This implementation provides the wrong values, likely when it
+    # comes down to hands involving different ranks.
 
-    hand1_values = [get_card_value(card, STANDARD_RANKS) for card in hand1]
-    hand2_values = [get_card_value(card, STANDARD_RANKS) for card in hand2]
-    differences = (v1 - v2 for v1, v2 in zip(hand1_values, hand2_values))
+    joker_count = card_counts.get(Card("J"), 0)
+    max_non_joker_count = max(
+        (card_counts[card] for card in card_counts if card != Card("J")),
+        default=0,
+    )
 
-    return next((diff for diff in differences if diff != 0), 0)
+    largest_same_rank_count = max_non_joker_count + joker_count
+
+    if largest_same_rank_count == 5:
+        return HandType.FIVE_OF_A_KIND
+
+    if largest_same_rank_count == 4:
+        return HandType.FOUR_OF_A_KIND
+
+    can_make_full_house = largest_same_rank_count >= 3 and len(card_counts) >= 2
+    if can_make_full_house:
+        return HandType.FULL_HOUSE
+
+    if largest_same_rank_count == 3:
+        return HandType.THREE_OF_A_KIND
+
+    if len(card_counts) == 3:
+        return HandType.TWO_PAIR
+
+    if largest_same_rank_count == 2:
+        return HandType.ONE_PAIR
+
+    return HandType.HIGH_CARD
+
+
+STANDARD_RULESET = Ruleset(
+    get_standard_hand_type,
+    {
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    },
+)
+
+JACKS_ARE_JOKERS_RULESET = Ruleset(
+    get_wild_hand_type,
+    {
+        "J": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "Q": 12,
+        "K": 13,
+        "A": 14,
+    },
+)
+
+
+def read_plays(file_path: str) -> list[Play]:
+    """Read the plays for a game of Camel Cards from the input file."""
+
+    with open(file_path) as file:
+        return [parse_play(line) for line in file]
+
+
+def parse_play(line: str) -> Play:
+    """Parse a play from the given line."""
+
+    raw_cards, raw_bid = line.strip().split(" ")
+
+    hand = tuple(Card(raw_card) for raw_card in raw_cards)
+    bid = int(raw_bid)
+
+    return Play(hand, bid)
 
 
 def total_winnings(
     plays: list[Play],
-    get_hand_type: Callable[[Hand], HandType],
+    ruleset: Ruleset,
 ) -> int:
     """Calculate the total winnings from the given hands and bids."""
 
     key = cmp_to_key(
-        lambda play1, play2: compare_cards(play1.hand, play2.hand, get_hand_type),
+        lambda play1, play2: compare_cards(play1.hand, play2.hand, ruleset),
     )
 
     sorted_by_rank = sorted(plays, key=key)
@@ -154,9 +222,21 @@ def main() -> None:
 
     plays = read_plays(file_path)
 
-    total_winnings_with_standard_rules = total_winnings(plays, get_standard_hand_type)
-    print("Total Winnings with Standard Rules: ", total_winnings_with_standard_rules)
+    standard_rule_total_winnings = total_winnings(plays, STANDARD_RULESET)
+    print("Using standard rules:")
+    print(f"Total winnings are {standard_rule_total_winnings}")
+    print()
+
+    total_winnings_with_jokers = total_winnings(plays, JACKS_ARE_JOKERS_RULESET)
+    print("Using Jacks Are Jokers rules:")
+    print(f"Total winnings are {total_winnings_with_jokers}")
 
 
 if __name__ == "__main__":
     main()
+
+
+# 250433274 is too low
+# 250637576 is wrong
+# 250740442 is wrong
+# 251363551 is incorrect
